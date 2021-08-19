79c6c6143fe952137d7fe1ec30809df79a56297e
Eloagbawe <eloagbawe@gmail.com>
2021-08-19 23:48:54 +0100

    connected database

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-const express = require("express");
+require('dotenv').config()
+const express = require('express');
 const app = express();
-const fs = require('fs');
 const path = require('path');
-const router = express.Router();
+const Contact = require('./models/contact');
+const connectDB = require("./db");
+
+connectDB();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+app.post('/submit-message', async function (req, res) {
+    const { name, email, subject,  message} = req.body;
+    if (!email || !name || !subject || !message) res.status(400).send('All fields are required')
+    const newContact = await Contact.create({
+        name,
+        email,
+        subject,
+        message
+    })
+    if (newContact) {
+      console.log("contact message created")
+      return res.status(201).send(newContact)
+    }
+    console.log("error")
+    return res.status(400).send('something went wrong')
+  })
+
+app.listen(3000);
 
 
-const port = 3000;
+
+// const express = require("express");
+// const app = express();
+// const fs = require('fs');
+// const path = require('path');
+// const router = express.Router();
 
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
-})
-
-app.use('/', router);
-app.use(express.static(__dirname + '/public'));
-
-app.use(express.urlencoded({
-    extended: true
-  }))
+// const port = 3000;
 
 
-console.log("Elo Agbawe Idiodi");
+// app.listen(port, () => {
+//   console.log(`Server listening at http://localhost:${port}`)
+// })
 
-app.post('/submit-message', (req, res) => {
-    // const name = req.body.name;
-    // const email = req.body.email;
-    // const subject = req.body.subject;
-    // const message = req.body.message;
-    console.log(req.body);
-    res.send(req.body);
+// app.use('/', router);
+// app.use(express.static(__dirname + '/public'));
+
+// app.use(express.urlencoded({
+//     extended: true
+//   }))
+
+
+// console.log("Elo Agbawe Idiodi");
+
+// app.post('/submit-message', (req, res) => {
+//     // const name = req.body.name;
+//     // const email = req.body.email;
+//     // const subject = req.body.subject;
+//     // const message = req.body.message;
+//     console.log(req.body);
+//     res.send(req.body);
     
 
-    //...
-    res.end()
-  })
+//     //...
+//     res.end()
+//   })
